a9175601c4a19906d612c54175df372e90d7920b
nyx <thanhnam.awa@gmail.com>
2024-09-29 16:14:48 +0700

    feat: add basic openapi configuration

--- src/main/java/com/easyhostel/backend/api/controller/ManagerController.java
+++ src/main/java/com/easyhostel/backend/api/controller/ManagerController.java
@@ -1,15 +1,19 @@
 package com.easyhostel.backend.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v0.1/user")
-public class UserController {
+@RequestMapping("/api/v0.1/manager")
+@Tag(name = "Manager Controller")
+public class ManagerController {
 
     @GetMapping
-    public String getUser() {
+    @Operation(summary = "Get manager's information", description = "")
+    public String getManager() {
         return "Hello World";
     }
 
---
+++ src/main/java/com/easyhostel/backend/infrastructure/configuration/OpenApiConfig.java
@@ -0,0 +1,49 @@
+package com.easyhostel.backend.infrastructure.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi(@Value("${open.api.title}") String title,
+                                 @Value("${open.api.version}") String version,
+                                 @Value("${open.api.description}") String description,
+                                 @Value("${open.api.serverUrl}") String serverUrl,
+                                 @Value("${open.api.serverName}") String serverName
+    ) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(title)
+                        .version(version)
+                        .description(description))
+                .servers(List.of(new Server()
+                        .url(serverUrl)
+                        .description(serverName)));
+//                // Add token authorization
+//                .components(new Components().addSecuritySchemes(
+//                        "bearerAuth",
+//                        new SecurityScheme()
+//                                .type(SecurityScheme.Type.HTTP)
+//                                .scheme("bearer")
+//                                .bearerFormat("JWT")))
+//                .security(List.of(new SecurityRequirement().addList("bearerAuth")));
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("API Service")
+                .packagesToScan("com.easyhostel.backend.api.controller")
+                .build();
+    }
+
+}

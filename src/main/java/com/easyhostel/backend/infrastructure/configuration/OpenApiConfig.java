19f11910559e776902c5987b67afdcda9461d036
nyx <thanhnam.awa@gmail.com>
2024-10-25 03:16:19 +0700

    feat: add bearer token access to Swagger UI

--- src/main/java/com/easyhostel/backend/infrastructure/configuration/OpenApiConfig.java
+++ src/main/java/com/easyhostel/backend/infrastructure/configuration/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.easyhostel.backend.infrastructure.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,21 +30,21 @@ public class OpenApiConfig {
                         .description(description))
                 .servers(List.of(new Server()
                         .url(serverUrl)
-                        .description(serverName)));
-//                // Add token authorization
-//                .components(new Components().addSecuritySchemes(
-//                        "bearerAuth",
-//                        new SecurityScheme()
-//                                .type(SecurityScheme.Type.HTTP)
-//                                .scheme("bearer")
-//                                .bearerFormat("JWT")))
-//                .security(List.of(new SecurityRequirement().addList("bearerAuth")));
+                        .description(serverName)))
+                // Add token authorization
+                .components(new Components().addSecuritySchemes(
+                        "bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .security(List.of(new SecurityRequirement().addList("bearerAuth")));
     }
 
     @Bean
     public GroupedOpenApi groupedOpenApi() {
         return GroupedOpenApi.builder()
-                .group("API Service")
+                .group("EasyHostel API Service")
                 .packagesToScan("com.easyhostel.backend.api.controller")
                 .packagesToExclude("com.easyhostel.backend.api.controller.base")
                 .build();
